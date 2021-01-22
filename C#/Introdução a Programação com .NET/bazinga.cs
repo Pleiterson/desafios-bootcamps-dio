f1ed09d8af9dada5018f1df92032d2ac2e9f82f2
pleiterson <pleiterson@gmail.com>
2021-01-22 11:05:31 -0300

    incluido os codigos do bootcamp .net fundamentals

---
+++ C#/Introdução a Programação com .NET/bazinga.cs
@@ -0,0 +1,122 @@
+// Bazinga!
+
+/* Em um episódio da aclamada série The Big Ban Theor dois pernagens, Sheldon e Raj, discutem qual dos dois é o melhor: o filme Saturn 3 ou a série Deep Space 9. A sugestão de Raj para a resolução do impasse é uma disputa de Pedra-Papel-Tesoura. Contudo, Sheldon argumenta que, se as partes envolvidas se conhecem, entre 75% e 80% das disputas de Pedra-Papel-Tesoura terminam empatadas, e então sugere o Pedra-Papel-Tesoura-Lagarto-Spock.
+
+As regras do jogo proposto são:
+
+1. a tesoura corta o papel;
+2. o papel embrulha a pedra;
+3. a pedra esmaga o lagarto;
+4. o lagarto envenena Spock;
+5. Spock destrói a tesoura;
+6. a tesoura decapita o lagarto;
+7. o lagarto come o papel;
+8. o papel contesta Spock;
+9. Spock vaporiza a pedra;
+10. a pedra quebra a tesoura.
+
+Conhecendo os personagens, sabemos que caso Sheldon vencesse, ele gritaria a vitória com um "Bazinga!". Se Raj vencesse, Sheldon o acusaria de ter trapaceado. Agora, se desse empate, Sheldon não descansaria e insistira para jogarem de novo até que seja decidido. Sabendo dessas ações, faça um programa que imprima a provável reação de Sheldon.
+
+- Entrada
+
+A entrada consiste em uma série de casos de teste. A primeira linha contém um inteiro positivo T (T ≤ 100), que representa o número de casos de teste. Cada caso de teste é representado por uma linha da entrada, contendo as escolhas de Sheldon e Raj, respectivamente, separadas por um espaço em branco. As escolhas possíveis são as personagens do jogo: pedra, papel, tesoura, lagarto e Spock.
+
+- Saida
+
+Para cada caso de teste deverá ser impressa a mensagem "Caso #t: R", onde t é o número do caso de teste (cuja contagem se inicia no número um) e R é uma das três reações possíveis de Sheldon: "Bazinga!", "Raj trapaceou!", ou "De novo!". */
+
+using System;
+
+class Desafio {
+    static void Main() {
+        int limit = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= limit; i++)
+            {
+                if (i > 100) return;
+                string[] line = Console.ReadLine().Split(" ");
+                string sheldon = line[0].ToLower();
+                string raj = line[1].ToLower();
+
+                switch (sheldon)
+                {
+                    case "pedra":
+                        switch (raj)
+                        {
+                            case "lagarto":
+                            case "tesoura":
+                                Console.WriteLine("Caso #" + i + ": Bazinga!");
+                                break;
+                            case "pedra":
+                                Console.WriteLine("Caso #" + i + ": De novo!");
+                                break;
+                            default:
+                                Console.WriteLine("Caso #" + i + ": Raj trapaceou!");
+                                break;
+                        }
+                        break;
+                    case "papel":
+                        switch (raj)
+                        {
+                            case "pedra":
+                            case "spock":
+                                Console.WriteLine("Caso #" + i + ": Bazinga!");
+                                break;
+                            case "papel":
+                                Console.WriteLine("Caso #" + i + ": De novo!");
+                                break;
+                            default:
+                                Console.WriteLine("Caso #" + i + ": Raj trapaceou!");
+                                break;
+                        }
+                        break;
+                    case "tesoura":
+                        switch (raj)
+                        {
+                            case "papel":
+                            case "lagarto":
+                                Console.WriteLine("Caso #" + i + ": Bazinga!");
+                                break;
+                            case "tesoura":
+                                Console.WriteLine("Caso #" + i + ": De novo!");
+                                break;
+                            default:
+                                Console.WriteLine("Caso #" + i + ": Raj trapaceou!");
+                                break;
+                        }
+                        break;
+                    case "lagarto":
+                        switch (raj)
+                        {
+                            case "spock":
+                            case "papel":
+                                Console.WriteLine("Caso #" + i + ": Bazinga!");
+                                break;
+                            case "lagarto":
+                                Console.WriteLine("Caso #" + i + ": De novo!");
+                                break;
+                            default:
+                                Console.WriteLine("Caso #" + i + ": Raj trapaceou!");
+                                break;
+                        }
+                        break;
+                    case "spock":
+                        switch (raj)
+                        {
+                            case "tesoura":
+                            case "pedra":
+                                Console.WriteLine("Caso #" + i + ": Bazinga!");
+                                break;
+                            case "spock":
+                                Console.WriteLine("Caso #" + i + ": De novo!");
+                                break;
+                            default:
+                                Console.WriteLine("Caso #" + i + ": Raj trapaceou!");
+                                break;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+    }
+}
---
+++ C#/Introdução a Programação com .NET/coxinha-de-bueno.cs
@@ -0,0 +1,26 @@
+// Coxinha de Bueno
+
+/* Em 2015 um novo record foi alcançado na competição de Coxinhas de Bueno de Andrada, onde Mônica mandou pra dentro 43 coxinhas em apenas 10 minutos, passando se antecessor que conseguiu comer, no mesmo tempo, 38 coxinhas em 2014.
+O restaurante especializado em coxinhas do pequeno distrito de Bueno de Andrada, interior de São Paulo, organiza essa competição todos os anos, mas nunca conseguiram entrar para o livro dos recordes, o Guinness Book. Para isso, o restaurante precisa preencher informações sobre a competição, como o número de coxinhas consumidas pelos competidores durante o evento. 
+Porém, como já foi informado, a especialidade deles é coxinha, não matemática, então será que você pode ajudá-los? Com base no número total de coxinhas consumidas e o número de participantes na competição, o dono do restaurante precisa que você desenvolva um programa para saber a quantidade média de coxinha que os participantes da competição conseguem devorar.
+Ah, lembre-se que, em troca da sua ajuda, você poderá comer quantas coxinhas conseguir.
+
+- Entrada
+
+A entrada consiste de uma única linha que contém dois inteiros H e P (1 ≤ H, P ≤ 1000) indicando respectivamente o número total de coxinhas consumidas e o número total de participantes na competição.
+
+- Saída
+
+Seu programa deve produzir uma única linha com um número racional representando o número médio de coxinhas consumidas pelos participantes. O resultado deve ser escrito como um número racional com exatamente dois dígitos após o ponto decimal, arredondado se necessário. */
+
+using System;
+
+class Desafio {
+    static void Main() {
+        string[] line = Console.ReadLine().Split(" ");
+        double coxinhas = int.Parse(line[0]);
+        double participantes = int.Parse(line[1]);
+        double media = coxinhas / participantes;
+        Console.WriteLine(media.ToString("N2"));
+    }
+}
